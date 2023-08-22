e5644480c20ac2795b75a35b440d4d4cfff2cc64
pavlo <pavlo.kulka@gmail.com>
2023-08-22 20:56:15 +0200

    First file to my TelegramBot

--- CryptoFatherBot.py
+++ CryptoFatherBot.py
@@ -1,10 +1,35 @@
 import logging
+import requests
 from telegram import Update, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 # Ініціалізація логгера
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+# Функція для отримання курсу валют
+def get_exchange_rate(currency):
+    url = f"https://api.exchangerate-api.com/v4/latest/USD"  # Замініть USD на валюту, яку ви хочете отримати
+    response = requests.get(url)
+    data = response.json()
+
+    if currency in data['rates']:
+        return data['rates'][currency]
+    else:
+        return "Невідома валюта"
+
+
+# Функція для отримання курсу криптовалют
+def get_crypto_price(crypto):
+    url = f"https://api.coingecko.com/api/v3/simple/price?ids={crypto}&vs_currencies=usd"
+    response = requests.get(url)
+    data = response.json()
+
+    if crypto in data and 'usd' in data[crypto]:
+        return data[crypto]['usd']
+    else:
+        return "Невідома криптовалюта"
 
 
 # Функція для обробки команди /start
@@ -33,7 +58,21 @@ def exchange(update: Update, context: CallbackContext) -> None:
     ]
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
 
-    update.message.reply_text("Оберіть валюту:", reply_markup=reply_markup)
+    update.message.reply_text("Оберіть валюту або криптовалюту:", reply_markup=reply_markup)
+
+
+# Функція для обробки вибору валюти або криптовалюти з клавіатури
+def selected_currency(update: Update, context: CallbackContext) -> None:
+    selected_currency = update.message.text
+    if selected_currency in ["USD", "EUR", "Bitcoin", "Ethereum"]:
+        if selected_currency in ["Bitcoin", "Ethereum"]:
+            price = get_crypto_price(selected_currency.lower())
+            update.message.reply_text(f"Поточна ціна {selected_currency}: {price} USD")
+        else:
+            rate = get_exchange_rate(selected_currency)
+            update.message.reply_text(f"Поточний курс {selected_currency}: {rate}")
+    else:
+        update.message.reply_text("Невірно обрано валюту або криптовалюту.")
 
 
 # Функція для обробки команди /weather
@@ -56,11 +95,11 @@ def main():
     # Додавання обробників команд
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("commands", commands))
-    dispatcher.add_handler(CommandHandler("exchange", exchange))  # Додано цю строку
-    dispatcher.add_handler(CommandHandler("weather", weather))  # Додано цю строку
+    dispatcher.add_handler(CommandHandler("exchange", exchange))
+    dispatcher.add_handler(CommandHandler("weather", weather))
 
-    # Додавання обробників повідомлень
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    # Додавання обробників вибору валюти або криптовалюти з клавіатури
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, selected_currency))
 
     # Запуск бота
     updater.start_polling()
